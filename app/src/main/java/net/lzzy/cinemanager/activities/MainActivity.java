8d3c31bb85c343cb2a644b802516ff75bdc87b1e
Airyi <2739327400@qq.com>
2019-03-26 15:29:22 +0800

    标题栏

--- app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/cinemanager/activities/MainActivity.java
@@ -1,19 +1,51 @@
 package net.lzzy.cinemanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 import net.lzzy.cinemanager.R;
 
 /**
  * @author Administrator
  */
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvTitle;
+    private View layoutMenu;
+    private SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        setTitleMenu();
+
+    }
+
+    private void setTitleMenu(){
+        layoutMenu = findViewById(R.id.bar_menu);
+        layoutMenu.setVisibility(View.GONE);
+        int visible = layoutMenu.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE;
+        layoutMenu.setVisibility(visible);
+        tvTitle = findViewById(R.id.bar_title_tv_title);
+        findViewById(R.id.bar_img_menu).setOnClickListener(this);
+        tvTitle.setText("我的订单");
+        search = findViewById(R.id.bar_searchView);
+        findViewById(R.id.bar_order).setOnClickListener(this);
+        findViewById(R.id.bar_add_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_add_order).setOnClickListener(this);
+        findViewById(R.id.bar_see_cinema).setOnClickListener(this);
+        findViewById(R.id.bar_exit).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
